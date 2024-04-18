5c72d15598013cce92ec786fdfafe00841d234c1
Kirill Mikheenko <meowmifi@yandex.ru>
2024-04-18 22:26:08 +0300

    changed files

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,15 +1,16 @@
 CREATE TABLE IF NOT EXISTS users
 (
-  usersid SERIAL,
-  role VARCHAR(100) NOT NULL,
-  first_name VARCHAR(100) NOT NULL,
-  last_name VARCHAR(100) NOT NULL,
-  email VARCHAR(100) NOT NULL,
-  father_name VARCHAR(100) NOT NULL,
-  password VARCHAR(100) NOT NULL,
-  passwordsalt VARCHAR(6) NOT NULL,
-  date_registration DATE NOT NULL,
-  UNIQUE (email)
+    usersid INT NOT NULL,
+    role VARCHAR(100) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    father_name VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    passwordsalt VARCHAR(6) NOT NULL,
+    date_registration DATE NOT NULL,
+    PRIMARY KEY (usersid),
+    UNIQUE (email)
 );
 
 CREATE TABLE IF NOT EXISTS teacher
@@ -22,8 +23,9 @@ CREATE TABLE IF NOT EXISTS teacher
 
 CREATE TABLE IF NOT EXISTS groups
 (
-    groupsid SERIAL,
-    groupsname VARCHAR(10)
+    groupsid INT NOT NULL,
+    groupsname VARCHAR(10),
+    PRIMARY KEY (groupsid)
 );
 
 CREATE TABLE IF NOT EXISTS student
@@ -48,9 +50,10 @@ CREATE TABLE IF NOT EXISTS tests
 
 CREATE TABLE IF NOT EXISTS teachergroup
 (
-    teachergroupid SERIAL,
+    teachergroupid INT NOT NULL,
     teacherid INT NOT NULL,
     groupsid INT NOT NULL,
+    PRIMARY KEY (teachergroupid),
     FOREIGN KEY (teacherid) REFERENCES teacher(teacherid),
     FOREIGN KEY (groupsid) REFERENCES groups(groupsid)
 );
@@ -59,37 +62,41 @@ CREATE TABLE IF NOT EXISTS result
 (
     time_start TIMESTAMP NOT NULL,
     time_end  TIMESTAMP NOT NULL,
-    resultid SERIAL,
+    resultid INT NOT NULL,
     sum_grade INT NOT NULL,
     studentid INT NOT NULL,
     testsid INT NOT NULL,
+    PRIMARY KEY (resultid),
     FOREIGN KEY (studentid) REFERENCES student(studentid),
     FOREIGN KEY (testsid) REFERENCES tests(testsid)
 );
 
 CREATE TABLE IF NOT EXISTS task
 (
-    taskid SERIAL,
+    taskid INT NOT NULL,
     task_name VARCHAR(100) NOT NULL,
     answer JSONB NOT NULL,
     data JSONB NOT NULL,
     max_grade INT NOT NULL,
-    description VARCHAR (1000) NOT NULL
+    description VARCHAR (1000) NOT NULL,
+    PRIMARY KEY (taskid)
 );
 
 CREATE TABLE IF NOT EXISTS test_task
 (
-    testtaskid SERIAL,
+    testtaskid INT NOT NULL,
     testsid INT NOT NULL,
     taskid INT NOT NULL,
+    PRIMARY KEY (testtaskid),
     FOREIGN KEY (testsid) REFERENCES tests(testsid),
     FOREIGN KEY (taskid) REFERENCES task(taskid)
 );
 
 CREATE TABLE grade
 (
-    gradeid SERIAL,
+    gradeid INT NOT NULL,
     grade INT NOT NULL,
     resultid INT NOT NULL,
+    PRIMARY KEY (gradeid),
     FOREIGN KEY (resultid) REFERENCES result(resultid)
 );
