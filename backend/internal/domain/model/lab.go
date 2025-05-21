f975413a4de621409313478f93dd66382f489ab4
dgt4l <nik.asiimov@mail.ru>
2025-05-21 20:22:05 +0300

    fix duration convertion

--- backend/internal/domain/model/lab.go
+++ backend/internal/domain/model/lab.go
@@ -1,21 +1,30 @@
 package model
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Lab struct {
-	ID               int64         `db:"lab_id" json:"id"`
-	Name             string        `db:"name" json:"name"`
-	Description      string        `db:"description" json:"description"`
-	Duration         time.Duration `db:"duration" json:"duration"`
-	RegistrationDate time.Time     `db:"registration_date" json:"registrationDate"`
-	TeacherID        int64         `db:"teacher_id" json:"teacherId"`
-	TeacherFIO       string        `db:"teacher_fio" json:"teacherFio"`
+	ID               int64     `db:"lab_id" json:"id"`
+	Name             string    `db:"name" json:"name"`
+	Description      string    `db:"description" json:"description"`
+	Duration         string    `db:"duration" json:"duration"`
+	RegistrationDate time.Time `db:"registration_date" json:"registrationDate"`
+	TeacherID        int64     `db:"teacher_id" json:"teacherId"`
+	TeacherFIO       string    `db:"teacher_fio" json:"teacherFio"`
 }
 
-func (l *Lab) SetDurationMinutes(durationMinutes int64) {
-	l.Duration = time.Duration(durationMinutes) * time.Minute
+func (l *Lab) SetDurationMinutes(minutes int64) {
+	h := minutes / 60
+	m := minutes % 60
+	l.Duration = fmt.Sprintf("%02d:%02d:00", h, m)
 }
 
 func (l *Lab) GetDurationMinutes() int64 {
-	return int64(l.Duration.Minutes())
+	t, err := time.Parse("15:04:05", l.Duration)
+	if err != nil {
+		return 0
+	}
+	return int64(t.Hour()*60 + t.Minute())
 }
